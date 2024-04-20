72c5e62c6eeb6a4d5e6d3ff723015602adb19289
Haidar <haidarah134@gmail.com>
2024-04-20 08:57:24 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302220152/BankTransferConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace modul8_1302220152
+{
+    internal class BankTransferConfig
+    {
+        public Config config;
+
+        // public string lang { get { return config.lang; } set { config.lang = new; } }
+
+        public BankTransferConfig()
+        {
+            string jsText = File.ReadAllText("../../../../bank_transfer_config.json");
+
+            var options = new JsonSerializerOptions
+            {
+                IncludeFields = true
+            };
+            config = JsonSerializer.Deserialize<Config>(jsText, options);
+        }
+
+    }
+    public class Config
+    {
+        public string lang;
+        public Transfer transfer;
+        public string[] methods;
+        public Confirmation confirmation;
+    }
+    public class Transfer
+    {
+        public int threshold;
+        public int low_fee;
+        public int high_fee;
+    }
+    public class Confirmation
+    {
+        public string en;
+        public string id;
+    }
+}
--- modul8_1302220152/Program.cs
+++ modul8_1302220152/Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankTransferConfig config = new BankTransferConfig();
+
+            if (config.config.lang == "en")
+                Console.WriteLine("Please insert the amount of money to transfer:");
+            else Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+
+
+            int amount = int.Parse(Console.ReadLine());
+
+            int biayatransfer;
+            if (amount <= config.config.transfer.threshold)
+                biayatransfer = config.config.transfer.low_fee;
+            else
+                biayatransfer = config.config.transfer.high_fee;
+
+            int totalAmount = amount + biayatransfer;
+
+            if (config.config.lang == "en")
+                Console.WriteLine($"Transfer fee = {biayatransfer}\nTotal amount = {totalAmount}");
+            else Console.WriteLine($"Biaya transfer = {biayatransfer}\nTotal biaya = {totalAmount}");
+
+            if (config.config.lang == "en")
+                Console.WriteLine("Select transfer method:");
+            else Console.WriteLine("Pilih metode transfer:");
+
+            int i = 1;
+            foreach (string mtd in config.config.methods)
+            {
+                Console.WriteLine($"{i}. {mtd}");
+                i++;
+            }
+            string method = Console.ReadLine();
+
+            string confirm;
+            if (config.config.lang == "en")
+            {
+                Console.WriteLine($"Please type {config.config.confirmation.en} to confirm transaction:");
+                confirm = Console.ReadLine();
+                if (confirm == config.config.confirmation.en)
+                    Console.WriteLine("The transfer is completed");
+                else Console.WriteLine("The transfer is cancelled");
+            }
+            else
+            {
+                Console.WriteLine($"Ketik {config.config.confirmation.id} untuk mengkonfirmasi transaksi:");
+                confirm = Console.ReadLine();
+                if (confirm == config.config.confirmation.id)
+                    Console.WriteLine("Proses transfer berhasil");
+                else Console.WriteLine("Transfer dibatalkan");
+            }
+
+
         }
     }
 }
